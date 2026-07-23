0d35bf54687195375f0c94b99a2eab6c02f93601
Yusei Yamashita <yusei.yamashita@gmail.com>
2026-07-23 21:39:23 +0900

    Day14 Local Storageで視聴状態を保存

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const button = document.getElementById("completeBtn");
 
-let completed = false;
+let completed = localStorage.getItem("video1") === "true";
+
+if (completed) {
+    button.textContent = "☑ 視聴済み";
+    button.classList.add("completed");
+} else {
+    button.textContent = "☐ 視聴済みにする";
+    button.classList.remove("completed");
+}
 
 button.addEventListener("click", function () {
 
     completed = !completed;
+
+    localStorage.setItem("video1", completed);
 
     if (completed) {
         button.textContent = "☑ 視聴済み";
